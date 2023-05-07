a334ecd2bff7eedea4098bf073c4ce6ff2dfcd46
linda <lidiyagez@gmail.com>
2023-05-07 07:52:36 -0700

    web dynamics

--- web_dynamic/100-hbnb.py
+++ web_dynamic/100-hbnb.py
@@ -10,42 +10,42 @@ from models.amenity import Amenity
 from models.place import Place
 from os import environ
 from flask import Flask, render_template
-# flask setup
+
+
 app = Flask(__name__)
-app.url_map.strict_slashes = False
 
 
 
-# begin flask page rendering
+
 @app.teardown_appcontext
 def close_db(error):
     """
-    this method calls .close() (i.e. .remove()) on
+    this method calls .close() or remove() on
     the current SQLAlchemy Session
     """
     storage.close()
 
 
-@app.route('/100-hbnb/')
+@app.route('/4-hbnb', strict_slashes=false)
 def hbnb(the_id=None):
     """
-    handles request to custom template with states, cities & amentities
+     HBNB is alive!
     """
-    states = storage.all('State').values()
-    amentities = storage.all('Amenity').values()
+    state_objs = storage.all('State').values()
+    states = dict([state.name, state] for state in state_objs)
+    amenities = storage.all('Amenity').values()
     places = storage.all('Place').values()
     users = dict([user.id, "{} {}".format(user.first_name, user.last_name)]
                  for user in storage.all('User').values())
     cache_id = (str(uuid.uuid4()))
     return render_template('100-hbnb.html',
                            states=states,
-                           amentities=amentities,
+                           amenities=amenities,
                            places=places,
                            users=users,
                            cache_id=cache_id)
 
 
 if __name__ == "__main__":
-    """
-    MAIN FLASK"""
+    """main FLASK"""
     app.run(host='0,0,0,0', port='5000')
