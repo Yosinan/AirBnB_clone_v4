bbcbc0b32711437a6a3b8899024b5998fca8b02a
linda <lidiyagez@gmail.com>
2023-05-07 08:00:25 -0700

    web dynamics

---
+++ web_dynamic/101-hbnb.py
@@ -0,0 +1,51 @@
+#!/usr/bin/python3
+"""
+Flask App that integrates with AirBnB static HTML Template
+"""
+import uuid
+from models import storage
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.place import Place
+from os import environ
+from flask import Flask, render_template
+
+
+app = Flask(__name__)
+
+
+
+
+@app.teardown_appcontext
+def close_db(error):
+    """
+    this method calls .close() or remove() on
+    the current SQLAlchemy Session
+    """
+    storage.close()
+
+
+@app.route('/101-hbnb', strict_slashes=false)
+def hbnb(the_id=None):
+    """
+     HBNB is alive!
+    """
+    state_objs = storage.all('State').values()
+    states = dict([state.name, state] for state in state_objs)
+    amenities = storage.all('Amenity').values()
+    places = storage.all('Place').values()
+    users = dict([user.id, "{} {}".format(user.first_name, user.last_name)]
+                 for user in storage.all('User').values())
+    cache_id = (str(uuid.uuid4()))
+    return render_template('101-hbnb.html',
+                           states=states,
+                           amenities=amenities,
+                           places=places,
+                           users=users,
+                           cache_id=cache_id)
+
+
+if __name__ == "__main__":
+    """main fun"""
+    app.run(host='0,0,0,0', port='5000')
